a8cd108aad50c0a60f6ff649f86f3054b4d21428
Debbie Hugger <debbie.hugger@gmail.com>
2016-08-03 15:46:43 +0100

    added review model

--- Gemfile
+++ Gemfile
@@ -43,6 +43,12 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'bootstrap-sass'
+  gem 'font-awesome-sass'
+  gem 'simple_form'
+  gem 'autoprefixer-rails'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 gem 'rspec-rails', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Crazy Wolf",
+    address:      "Riponne, Lausanne",
+    phone_number: "079 264 56 32",
+    category:     "french"
+  },
+  {
+    name:         "Great Escape",
+    address:      "Bessières, Lausanne",
+    phone_number: "078 654 32 89",
+    category:     "belgian"
+  },
+  {
+    name:         "Cafe Bellini",
+    address:      "Riponne, Lausanne",
+    phone_number: "076 456 24 39",
+    category:     "italian"
+  },
+  {
+    name:         "Les Brasseurs",
+    address:      "Flon, Lausanne",
+    phone_number: "079 128 45 87",
+    category:     "belgian"
+  },
+  {
+    name:         "Chez Xu",
+    address:      "Lausanne Gare",
+    phone_number: "076 876 54 34",
+    category:     "chinese"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
